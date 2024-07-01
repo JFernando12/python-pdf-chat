bbd2ecd7b055d60ec84ec7546d04cb17b5255a3d
Fernando Castrejon <fernandocastrejonh@gmail.com>
2024-07-01 14:11:40 -0600

    changes

--- frontend/src/components/PdfViewer.tsx
+++ frontend/src/components/PdfViewer.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
+import React from 'react';
+import { pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
